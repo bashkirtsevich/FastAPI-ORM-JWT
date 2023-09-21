4deda8560a071b5877d8722736852b479b22c916
D.A.Bashkirtsev <bashkirtsevich@users.noreply.github.com>
2023-09-21 17:27:15 +0500

    Update abstract.py

--- app/models/abstract.py
+++ app/models/abstract.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
 import datetime
+from typing import AsyncIterator
 
 from sqlalchemy import DateTime
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
@@ -12,6 +17,25 @@ class AbstractModel(Base):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column("id", autoincrement=True, nullable=False, unique=True, primary_key=True)
+
+    @classmethod
+    async def _list(cls, session: AsyncSession) -> AsyncIterator[AbstractModel]:
+        query = select(cls)
+
+        stream = await session.stream_scalars(query.order_by(cls.id))
+        async for row in stream:
+            yield row
+
+    @classmethod
+    async def _get(cls, session: AsyncSession, id: int) -> AbstractModel | None:
+        query = select(cls).where(cls.id == id)
+
+        return await session.scalar(query.order_by(cls.id))
+
+    @classmethod
+    async def _delete(cls, session: AsyncSession, obj: AbstractModel) -> None:
+        await session.delete(obj)
+        await session.flush()
 
 
 class TimestampedModel(AbstractModel):
